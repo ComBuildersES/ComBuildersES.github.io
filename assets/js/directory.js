bb9eec1e52c6dff1118eaf120f4de9affc83f7c2
Jorge Barrachina <nusquema@gmail.com>
2016-02-14 12:33:39 +0100

    Fixed Issue #7

--- assets/js/directory.js
+++ assets/js/directory.js
@@ -3,23 +3,14 @@
 (function($) {
 
 	$.getJSON( 'data/communities.json', function( data ) {
-	  //var items = [];
-	  /*$.each( data.communities, function( key, val ) {
-	    items.push( '<li id="' + key + '">' + val.name + '</li>' );
-	  });*/
-	 
+
 	  var template = $.templates('#itemTmpl');
-
-		var htmlOutput = template.render(data.communities);
-
+		var htmlOutput = template.render(data.communities.sort(function(){ return 0.5 - Math.random(); }));
 		$('#community-list').html(htmlOutput);
-	  /*$( '<ul/>', {
-	    'class': 'my-new-list',
-	    html: items.join( '' )
-	  }).appendTo( '#content' );*/
+
 	});
 
-	
-	
+
+
 
 })(jQuery);
